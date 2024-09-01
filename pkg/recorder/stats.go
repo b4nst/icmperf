765ef41bae88840a96655950c12d6fd50ac22247
banst <bastien@loftorbital.com>
2024-09-01 14:17:19 +0200

    feat: use iqr filtering

--- pkg/recorder/stats.go
+++ pkg/recorder/stats.go
@@ -17,42 +17,65 @@ type Stats struct {
 	TotalReceived int
 }
 
+// iqrFilter filters the data using the Interquartile Range method.
+func iqrFilter(data []float64) []float64 {
+	sort.Float64s(data)
+	q1 := data[len(data)/4]
+	q3 := data[3*len(data)/4]
+	iqr := q3 - q1
+	lower := q1 - 1.5*iqr
+	upper := q3 + 1.5*iqr
+
+	lowerIndex := 0
+	for i, v := range data {
+		if v >= lower {
+			lowerIndex = i
+			break
+		}
+	}
+
+	upperIndex := len(data)
+	for i := len(data) - 1; i >= 0; i-- {
+		if data[i] <= upper {
+			upperIndex = i
+			break
+		}
+	}
+
+	return data[lowerIndex:upperIndex]
+}
+
+// mean calculates the arithmetic mean of a serie.
+func mean(data []float64) float64 {
+	var total float64
+	for _, v := range data {
+		total += v
+	}
+	return total / float64(len(data))
+}
+
 // Bandwidth returns the bandwidth in bytes per second.
-// 10% of the best and worst bandwidths are removed before calculating the mean.
+// The serie is IQR filtered, and mean is calculated.
 func (s *Stats) Bandwidth() float64 {
 	bws := make([]float64, 0, len(s.Bandwidths))
 	for _, v := range s.Bandwidths {
 		bws = append(bws, v)
 	}
-	sort.Float64s(bws)
-	offset := len(bws) / 10
-	bws = bws[offset : len(bws)-offset]
+	bws = iqrFilter(bws)
 
-	var total float64
-	for _, bandwidth := range bws {
-		total += bandwidth
-	}
-	return total / float64(len(bws))
+	return mean(bws)
 }
 
 // Latency returns the average latency.
-// 10% of the best and worst latencies are removed before calculating the mean.
+// The serie is IQR filtered, and mean is calculated.
 func (s *Stats) Latency() time.Duration {
-	latencies := make([]time.Duration, 0, len(s.Latencies))
+	latencies := make([]float64, 0, len(s.Latencies))
 	for _, v := range s.Latencies {
-		latencies = append(latencies, v)
+		latencies = append(latencies, v.Seconds())
 	}
-	sort.Slice(latencies, func(i, j int) bool {
-		return latencies[i] < latencies[j]
-	})
-	offset := len(latencies) / 10
-	latencies = latencies[offset : len(latencies)-offset]
+	latencies = iqrFilter(latencies)
 
-	var total time.Duration
-	for _, latency := range latencies {
-		total += latency
-	}
-	return total / time.Duration(len(latencies))
+	return time.Duration(mean(latencies) * float64(time.Second))
 }
 
 // PacketLoss returns the percentage of packets lost.
