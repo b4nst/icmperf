4341eab23d1805d2e1c830f5d8469dcc64980b0c
banst <bastien@loftorbital.com>
2024-09-04 19:02:08 +0200

    feat: add session

---
+++ pkg/session/session.go
@@ -0,0 +1,44 @@
+package session
+
+import (
+	"github.com/prometheus-community/pro-bing"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/b4nst/icmperf/pkg/recorder"
+)
+
+type Session struct {
+	// pingers is a list of pingers.
+	pingers []*probing.Pinger
+	// record is a session record.
+	record *recorder.Record
+}
+
+// NewSession creates a new session with the given number of pingers.
+func NewSession(pingers []*probing.Pinger) *Session {
+	s := &Session{
+		pingers: pingers,
+		record:  recorder.NewRecord(),
+	}
+	for _, pinger := range pingers {
+		pinger.OnRecv = func(p *probing.Packet) {
+			s.record.AddPacket(p)
+		}
+	}
+	return s
+}
+
+func (s *Session) Run() error {
+	wg := new(errgroup.Group)
+	for _, pinger := range s.pingers {
+		p := pinger // capture range variable
+		wg.Go(func() error {
+			return p.Run()
+		})
+	}
+	return wg.Wait()
+}
+
+func (s *Session) Statistics() []*recorder.Stat {
+	return s.record.Stats()
+}
