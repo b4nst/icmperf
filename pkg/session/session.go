920373a03a82021a095ccd12eef0c5305ac7bdde
banst <bastien@loftorbital.com>
2024-09-06 01:25:18 +0200

    refactor: remove unused code

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -3,27 +3,17 @@ package session
 import (
 	"github.com/prometheus-community/pro-bing"
 	"golang.org/x/sync/errgroup"
-
-	"github.com/b4nst/icmperf/pkg/recorder"
 )
 
 type Session struct {
 	// pingers is a list of pingers.
 	pingers []*probing.Pinger
-	// record is a session record.
-	record *recorder.Record
 }
 
 // NewSession creates a new session with the given number of pingers.
 func NewSession(pingers []*probing.Pinger) *Session {
 	s := &Session{
 		pingers: pingers,
-		record:  recorder.NewRecord(),
-	}
-	for _, pinger := range pingers {
-		pinger.OnRecv = func(p *probing.Packet) {
-			s.record.AddPacket(p)
-		}
 	}
 	return s
 }
